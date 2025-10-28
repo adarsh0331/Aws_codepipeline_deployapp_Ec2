8a26427439d3cced70f3e9d7811a80fd81d03e61
Barkunta Adarsh <129056162+adarsh0331@users.noreply.github.com>
2025-10-28 12:56:09 +0530

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,9 +1,2 @@
 // test/test.js
 console.log("Running tests...");
-
-// Dummy test example
-describe('Sample Test', () => {
-  it('should always pass', () => {
-    console.log('Test passed ✅');
-  });
-});
