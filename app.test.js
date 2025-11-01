a049450bf4c1c279c82f91f47317a495145d4702
Barkunta Adarsh <129056162+adarsh0331@users.noreply.github.com>
2025-11-01 15:59:11 +0530

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,2 +1,6 @@
-// test/test.js
-console.log("Running tests...");
+// simple smoke test
+const http = require('http');
+http.get({hostname:'localhost', port:3000, path:'/'}, res => {
+  console.log('STATUS', res.statusCode);
+  process.exit(0);
+}).on('error', (e) => { console.error('ERR', e); process.exit(1); });
