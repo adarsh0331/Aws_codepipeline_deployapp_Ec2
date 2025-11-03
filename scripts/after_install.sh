f679e58546043ed8afadd67b9dc42265c6a4fa07
Barkunta Adarsh <129056162+adarsh0331@users.noreply.github.com>
2025-11-03 17:50:34 +0530

    Refactor after_install script for app deployment
    Removed initial echo and npm install steps, added commands to unzip artifact and set permissions for the app directory.

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 set -e
-echo "===== After Install Script Started ====="
-
-cd /home/ec2-user/nodejsapp
-
-# Ensure correct permissions
-sudo chown -R ec2-user:ec2-user /home/ec2-user/nodejsapp
-chmod +x scripts/*.sh
-
-# Install npm dependencies
-npm install
-
-echo "===== After Install Completed ====="
-
+cd /home/ec2-user/app
+# unzip artifact.zip into folder
+unzip -o artifact.zip -d /home/ec2-user/app
+# ensure node_modules exist (we packaged node_modules from CodeBuild)
+chown -R ec2-user:ec2-user /home/ec2-user/app
