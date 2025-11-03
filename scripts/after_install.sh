6b6e2134a314c68bd037eb98d2b7414be4308c10
Barkunta Adarsh <129056162+adarsh0331@users.noreply.github.com>
2025-11-03 20:33:50 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 cd /home/ec2-user/app
-# unzip artifact.zip into folder
-unzip -o artifact.zip -d /home/ec2-user/app
+
 # ensure node_modules exist (we packaged node_modules from CodeBuild)
 chown -R ec2-user:ec2-user /home/ec2-user/app
