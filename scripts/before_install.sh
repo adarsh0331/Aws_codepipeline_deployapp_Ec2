301b3755baa021b2ed7096fa09c0ef863afcecc9
Barkunta Adarsh <129056162+adarsh0331@users.noreply.github.com>
2025-11-04 16:55:27 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-set -e
-echo "BeforeInstall: cleaning old files"
-rm -rf /home/ec2-user/app || true
-mkdir -p /home/ec2-user/app
+echo "Running BeforeInstall - cleaning up and setting permissions"
+
+# Clear old app files
+sudo rm -rf /home/ec2-user/app/*
+
+# Ensure scripts are executable
+sudo chmod +x /home/ec2-user/app/scripts/*.sh
