f5500a0b70e5d1356f2dda158e62743e4e077411
Barkunta Adarsh <129056162+adarsh0331@users.noreply.github.com>
2025-11-04 17:14:20 +0530

    Update permissions and install dependencies as ec2-user

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,8 +3,8 @@ echo "Running AfterInstall - setting permissions and installing dependencies"
 
 cd /home/ec2-user/app
 
-# Set execute permission after files are copied
-chmod +x scripts/*.sh
+sudo chown -R ec2-user:ec2-user /home/ec2-user/app
+sudo chmod +x scripts/*.sh
 
-# Install dependencies if Node.js project
-npm install
+# Install dependencies as ec2-user
+sudo -u ec2-user npm install
